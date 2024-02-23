dae0b9e43c2f7c14c2952bb50111f961cfbf288a
Paraxodon <88501462+Paraxodon@users.noreply.github.com>
2024-02-23 15:21:11 +0100

    REFACTORING

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 #include "raylib.h"
 #include "raymath.h"
-#include "second.h"
+#include "physac.h"
 #include <stdio.h>
 
 //------------------------------------------------------------------------------------
--- src/second.c
+++
@@ -1,6 +0,0 @@
-#include "second.h"
-#include <stdio.h>
-
-void Fun1(void){
-    printf("\n\n Printing Test \n\n");
-}
